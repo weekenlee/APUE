27dc20972d37d3abcba2e11084fa748ec79cf106
lwj1396 <lwj1396@163.com>
2018-07-20 18:09:57 +0800

    hole

--- Chapter3/3_2_create_file_with_hole_my.c
+++ Chapter3/3_2_create_file_with_hole_my.c
@@ -17,4 +17,31 @@ int main(void)
     if((fd = creat("file.hole", FILE_MODE)) < 0) {
         printf("create error\n");
     }
+
+    if(write(fd, buf1, 10) != 10) {
+        printf("write error"); 
+    }
+    //现在offset是10
+    
+    if(lseek(fd, 16394, SEEK_SET) == -1) {
+        printf("lseek error"); 
+    }
+    //现在offset是16384
+
+    if(write(fd, buf2, 10) != 10) {
+        printf("write error"); 
+    }
+    //现在offset是16394
+
+    close(fd);
+
+    if((fd = creat("file.nohole", FILE_MODE))<0) {
+        printf("create error\n");
+    }
+
+    if(write(fd, buf3, 16394) != 16394) {
+        printf("write error"); 
+    }
+
+    exit(0);
 }
